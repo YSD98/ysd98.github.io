24ecc717cca7e5c6d42c0e1af2a65f88310cc2fb
Yash Asus PC <62443679+YSD98@users.noreply.github.com>
2025-09-25 17:31:22 +0530

    Upload WIP

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Yash-3DPortfolio-0.1.0";
+const contentToCache = [
+    "Build/WIP.loader.js",
+    "Build/WIP.framework.js",
+    "Build/WIP.data",
+    "Build/WIP.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
